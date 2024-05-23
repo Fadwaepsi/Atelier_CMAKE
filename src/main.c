931e22d6e23e535b734cd253bff5fd1ea208b9a3
Fadwaepsi <166632745+Fadwaepsi@users.noreply.github.com>
2024-05-23 11:52:11 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -26,8 +26,8 @@ int main(int argc, char *argv[]){
 		r = _div(atof(a), atof(b));
 		printf("%lf",r);
 	}
-	else if (strcmp(op, "car") == 0) {
-		r = _car(atof(a));
+	else if (strcmp(op, "square") == 0) {
+		r = _square(atof(a));
 		printf("%lf",r);
 	}
 	else {printf("Erreur de parametres");}
